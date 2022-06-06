7ea6535d3cee4aa33d63ac434c8e6c67c873f867
seanlee611 <seanlee611@gmail.com>
2022-06-06 12:29:35 -0700

    KattisBasketball
    initial submission

---
+++ General/src/KattisBasketball.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class KattisBasketball {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		String input = in.nextLine();
+		input = input.substring(input.length()-2,input.length()-1);
+		System.out.println(input);
+
+	}
+
+}
