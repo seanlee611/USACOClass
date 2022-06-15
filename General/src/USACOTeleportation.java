d90e99121f795bd6af38456fac5da64b69b5037b
seanlee611 <seanlee611@gmail.com>
2022-06-14 17:46:29 -0700

    Update USACOTeleportation.java

--- General/src/USACOTeleportation.java
+++ General/src/USACOTeleportation.java
@@ -13,9 +13,16 @@ public class USACOTeleportation {
 		int x = Integer.parseInt(input[2]);
 		int y = Integer.parseInt(input[3]);
 		
+		//Case 1: a->b
 		int ans = Math.abs(a-b);
+		
+		//Case 2: a->x->y->b
 		ans = Math.min(ans,  Math.abs(a-x) + Math.abs(b-y));
+		
+		//Case 3: a->y->x->b
 		ans = Math.min(ans,  Math.abs(a-y) + Math.abs(b-x));
+		
+		
 		out.println(ans);
 		in.close();
 		out.close();
